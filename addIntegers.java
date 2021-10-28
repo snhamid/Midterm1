5fab59dc5d31a657b7d88020b58e232304d32ba9
snhamid <71673549+snhamid@users.noreply.github.com>
2021-10-28 16:42:14 -0400

    Update addIntegers.java

--- addIntegers.java
+++ addIntegers.java
@@ -2,7 +2,7 @@ public class addIntegers {
 
     public static void main(String[] args) {
 
-        int first = 10;
+        int first = 100;
         int second = 20;
 
         System.out.println("Enter two numbers: " + first + " " + second);
